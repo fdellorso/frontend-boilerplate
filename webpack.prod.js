322fdd03147b432c6cc22351b29d435dec81cf95
Francesco Dell'Orso <francesco.dellorso@gmail.com>
2022-03-29 08:04:52 +0200

    added terser minimizer plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,26 +1,27 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const CompressionPlugin = require('compression-webpack-plugin')
-const BrotliPlugin = require('brotli-webpack-plugin')
+// const CompressionPlugin = require('compression-webpack-plugin')
+// const BrotliPlugin = require('brotli-webpack-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
   plugins: [
-    new MiniCssExtractPlugin(),
-    new CompressionPlugin({
-      algorithm: 'gzip',
-      test: /\.js$|\.css$|\.html$/,
-      threshold: 8192,
-      minRatio: 0.8
-    }),
-    new BrotliPlugin({
-      asset: '[path].br[query]',
-      test: /\.js$|\.css$|\.html$/,
-      threshold: 8192,
-      minRatio: 0.8
-    })
+    new MiniCssExtractPlugin()
+    // new CompressionPlugin({
+    //   algorithm: 'gzip',
+    //   test: /\.js$|\.css$|\.html$/,
+    //   threshold: 8192,
+    //   minRatio: 0.8
+    // }),
+    // new BrotliPlugin({
+    //   asset: '[path].br[query]',
+    //   test: /\.js$|\.css$|\.html$/,
+    //   threshold: 8192,
+    //   minRatio: 0.8
+    // })
   ],
   module: {
     rules: [
@@ -31,22 +32,30 @@ module.exports = merge(common, {
     ]
   },
   optimization: {
+    minimize: true,
+    runtimeChunk: 'single',
     splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
       cacheGroups: {
-        commons: {
+        vendor: {
           test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all'
+          name (module) {
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
+            return `npm.${packageName.replace('@', '')}`
+          }
         }
       }
     },
     minimizer: [
+      new TerserPlugin(),
       new CssMinimizerPlugin()
     ]
     // usedExports: true
-  },
-  externals: {
-    'bootstrap.native/dist/bootstrap-native-v4': 'bootstrap.native',
-    hammerjs: 'hammerjs'
   }
+  // externals: {
+  //   'bootstrap.native/dist/bootstrap-native-v4': 'bootstrap.native',
+  //   hammerjs: 'hammerjs'
+  // }
 })
